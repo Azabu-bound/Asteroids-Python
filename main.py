239bdee6ba6db008f39ff16e286abea4cb6e3c13
Azabu-bound <tylerjbailey95@gmail.com>
2024-08-20 20:37:32 -0400

    cleaned up game loop

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   clock = pygame.time.Clock()
+  
+  updatable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+
+  Player.containers = (updatable, drawable)
+  
   player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
   dt = 0
   
@@ -17,10 +23,14 @@ def main():
       if event.type == pygame.QUIT:
         return
 
-    player.update(dt)
+    for obj in updatable:
+      obj.update(dt)
 
     screen.fill("black")
-    player.draw(screen)
+
+    for obj in drawable:
+      obj.draw(screen)
+ 
     pygame.display.flip()
     
     # limit the fps to 60
